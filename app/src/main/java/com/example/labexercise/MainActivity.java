de4864b2fa80f6b3c4d3b57b94f73287650bd32c
Sabin Joshi <Sabin@Sabin-Joshi.local>
2022-04-05 18:04:19 +0545

    ActivityLifeHomework

--- app/src/main/java/com/example/labexercise/MainActivity.java
+++ app/src/main/java/com/example/labexercise/MainActivity.java
@@ -1,85 +1,43 @@
 package com.example.labexercise;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-public class MainActivity extends AppCompatActivity {
-
-
-    public static final int ITEM_REQUEST = 1;
-    private final TextView[] item = new TextView[10];
-    private ArrayList<String> itemsList = new ArrayList<>(10);
+    private Button mButton;
+    private TextView mTextView;
+    private EditText mEdit;
+    int counter;
+    private static final String MAIN_ACTIVITY_STATE = "state";
+    String saveEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        item[0] = findViewById(R.id.item1);
-        item[1] = findViewById(R.id.item2);
-        item[2] = findViewById(R.id.item3);
-        item[3] = findViewById(R.id.item4);
-        item[4] = findViewById(R.id.item5);
-        item[5] = findViewById(R.id.item6);
-        item[6] = findViewById(R.id.item7);
-        item[7] = findViewById(R.id.item8);
-        item[8] = findViewById(R.id.item9);
-        item[9] = findViewById(R.id.item10);
 
-        if (savedInstanceState != null) {
-            itemsList = savedInstanceState.getStringArrayList("ItemsListStringArray");
-            int i;
-            if (itemsList != null && itemsList.size() > 0) {
-                for (i = 0; i < itemsList.size(); i++) {
-                    if (itemsList.size() > 10) {
-                        Toast.makeText(MainActivity.this, "Cannot add more items to the list", Toast.LENGTH_LONG).show();
-                        break;
-                    }
-                    item[i].setVisibility(View.VISIBLE);
-                    item[i].setText(itemsList.get(i));
-                }
-            }
-        }
+        mButton = findViewById(R.id.button);
+        mTextView = findViewById(R.id.textView);
+        mEdit = findViewById(R.id.edit);
+        counter = 0;
+        mButton.setOnClickListener(this);
+
+
     }
 
     @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        if (itemsList.size() != 0) {
-            outState.putStringArrayList("ItemsListStringArray", itemsList);
-        }
+    public void onClick(View view) {
+
+        counter++;
+        mTextView.setText(""+counter);
     }
 
-    public void addItem(View view) {
-        Intent intent = new Intent(this, SecondActivity.class);
-        startActivityForResult(intent, ITEM_REQUEST);
-    }
 
-    // method for when SecondActivity returns intent
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ITEM_REQUEST) {
-            if (resultCode == RESULT_OK) {
-                String itemString = data.getStringExtra(SecondActivity.EXTRA_ITEMS);
-                itemsList.add(itemString);
-                int i;
-                for (i = 0; i < itemsList.size(); i++) {
-                    if (itemsList.size() > 10) {
-                        Toast.makeText(MainActivity.this, "Cannot add more items to the list", Toast.LENGTH_LONG).show();
-                        break;
-                    }
-                    item[i].setVisibility(View.VISIBLE);
-                    item[i].setText(itemsList.get(i));
-                }
-
-            }
-        }
-    }
 }
