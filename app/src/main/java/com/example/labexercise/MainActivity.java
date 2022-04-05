24e2219854e727614364e9e3b3bbc135762bc8b8
Sabin Joshi <Sabin@Sabin-Joshi.local>
2022-04-05 17:52:34 +0545

    ActivityLifeCycle

--- app/src/main/java/com/example/labexercise/MainActivity.java
+++ app/src/main/java/com/example/labexercise/MainActivity.java
@@ -4,46 +4,140 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button mSayHello, mCount;
-    private TextView mTextView;
-    private int counter;
+public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    public static final String EXTRA_MESSAGE
+            = "com.example.android.twoactivities.extra.MESSAGE";
+    public static final int TEXT_REQUEST = 1;
+
+    private EditText mMessageEditText;
+    private TextView mReplyHeadTextView;
+    private TextView mReplyTextView;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "onStart");
+    }
+
+    /**
+     * Maintains the Activity state across configuration changes.
+     *
+     * @param outState Activity state data to save
+     */
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mReplyHeadTextView.getVisibility() == View.VISIBLE) {
+            outState.putBoolean("reply_visible", true);
+            outState.putString("reply_text", mReplyTextView.getText().toString());
+        }
+    }
+
+    /**
+     * Initializes the activity.
+     *
+     * @param savedInstanceState The current state data
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSayHello = findViewById(R.id.sayHello);
-        mCount = findViewById(R.id.count);
-        mTextView = findViewById(R.id.textView);
+        Log.d(LOG_TAG, "-------");
+        Log.d(LOG_TAG, "onCreate");
 
-        counter =0;
+        mMessageEditText = findViewById(R.id.editText_main);
+        mReplyHeadTextView = findViewById(R.id.text_header_reply);
+        mReplyTextView = findViewById(R.id.text_message_reply);
 
-        mSayHello.setOnClickListener(this);
-        mCount.setOnClickListener(this);
+        if (savedInstanceState != null) {
+            boolean isVisible = savedInstanceState
+                    .getBoolean("reply_visible");
+            if (isVisible) {
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+                mReplyTextView.setText(savedInstanceState
+                        .getString("reply_text"));
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
+    }
 
+    /**
+     * Handles the onClick for the "Send" button. Gets the value of the main
+     * EditText, creates an intent, and launches the second activity with
+     * that intent.
+     *
+     * The return intent from the second activity is onActivityResult().
+     *
+     * @param view The view (Button) that was clicked.
+     */
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivityForResult(intent, TEXT_REQUEST);
+    }
+
+    /**
+     * Handles the data in the return intent from SecondActivity.
+     *
+     * @param requestCode Code for the SecondActivity request.
+     * @param resultCode Code that comes back from SecondActivity.
+     * @param data Intent data sent back from SecondActivity.
+     */
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == TEXT_REQUEST) {
+            // Test to make sure the intent reply result was good.
+            if (resultCode == RESULT_OK) {
+                String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+
+
+                mReplyHeadTextView.setVisibility(View.VISIBLE);
+
+                mReplyTextView.setText(reply);
+                mReplyTextView.setVisibility(View.VISIBLE);
+            }
+        }
     }
 
     @Override
-    public void onClick(View view) {
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "onPause");
+    }
 
-        switch (view.getId())
-        {
-            case R.id.sayHello:
-                Intent intent = new Intent(MainActivity.this, HelloToast.class);
-                intent.putExtra("CounterValue", ""+counter);
-                startActivity(intent);
-                break;
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(LOG_TAG, "onRestart");
+    }
 
-            case R.id.count:
-                counter++;
-                mTextView.setText(""+counter);
-                break;
-        }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "onDestroy");
     }
 }
